190ae2695eba8d6f9e4f77a8014dfc482c236c44
RohitBiradar <rohitbiradar18@gmail.com>
2020-09-09 18:22:11 +0530

    Last commit of the day

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -1,10 +1,14 @@
- import { LightningElement, api } from 'lwc';
+import { LightningElement, api , wire } from 'lwc';
+import {registerListener} from 'c/pubsub';
+import { CurrentPageReference } from 'lightning/navigation';
 
 export default class MeetingRoom extends LightningElement {
-    @api meetingRoomInfo;
+    @api meetingRoomInfo = {roomName : 'A-01', roomCapacity : '12'};
     
+    @wire(CurrentPageReference) pageReference;
     tileclickhandler(){
         const tileClicked = new CustomEvent('tileclick', {detail : this.meetingRoomInfo, bubbles : true});
         this.dispatchEvent(tileClicked);
+        fireEvent(this.pageReference, 'pubsubtileclick', this.meetingRoomInfo);
     }
 }
---
+++ force-app/main/default/lwc/selectedMeetingRoom/selectedMeetingRoom.js
@@ -0,0 +1,20 @@
+import { LightningElement , track , wire} from 'lwc';
+import {registerListener, unregisterAllListeners} from 'c/pubsub';
+import { CurrentPageReference } from 'lightning/navigation';
+
+export default class SelectedMeetingRoom extends LightningElement {
+    @track selectedMeetingInfo;
+
+    @wire(CurrentPageReference) pageRef;
+    connectedCallback(){
+        registerListener('pubsubtileclick', this.onMeetingRoomSelectHandler, this)
+    }
+
+    disconnectedCallback(){
+        unregisterAllListeners(this);
+    }
+
+    onMeetingRoomSelectHandler(payload) {
+        this.selectedMeetingInfo
+    }
+}
