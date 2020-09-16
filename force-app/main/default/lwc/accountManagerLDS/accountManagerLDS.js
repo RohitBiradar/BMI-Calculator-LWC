44ff7e4f12f0717a03665795f315ef62788d9d1d
RohitBiradar <rohitbiradar18@gmail.com>
2020-09-16 17:47:32 +0530

    View record Commit

--- force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
+++ force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
@@ -1,10 +1,18 @@
-import { LightningElement , track} from 'lwc';
-import { createRecord } from "lightning/uiRecordApi";
+import { LightningElement , track , wire} from 'lwc';
+import { createRecord, getRecord } from "lightning/uiRecordApi";
+import Account from '@salesforce/schema/Case.Account';
+
+
+const fieldArray = ['Account.Name', 'Account.Phone', 'Account.Website'];
 
 export default class AccountManagerLDS extends LightningElement {
     @track accountName;
     @track accountPhone;
     @track accountWebsite;
+    @track recordId;
+
+    @wire(getRecord, {recordId: '$recordId', fieldArray})
+    accountRecord;
 
     accountNameChangeHandler(event) {
         this.accountName = event.target.value;
@@ -21,10 +29,29 @@ export default class AccountManagerLDS extends LightningElement {
         const recordInput = {apiName : 'Account' , fields};
         createRecord(recordInput).then(response => {
             console.log('Account has been created : ' + response.id);
+            this.recordId = response.id;
         }).catch(error => {
             console.log('Error in creating Account : ' + error.body.message);
         });
     }
 
-
+    get retAccountName() {
+        if(this.accountRecord.data) {
+            return this.accountRecord.data.fields.Name.value;
+        }
+        return undefined;
+    }
+    
+    get retAccountPhone() {
+        if(this.accountRecord.data) {
+            return this.accountRecord.data.fields.Phone.value;
+        }
+        return undefined;
+    }
+    get retAccountWebsite() {
+        if(this.accountRecord.data) {
+            return this.accountRecord.data.fields.Website.value;
+        }
+        return undefined;
+    }
 }
