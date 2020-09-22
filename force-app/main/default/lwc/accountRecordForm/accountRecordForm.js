f8e62ea3af527aa2d32e18ef29f7d2a1b1844cc0
RohitBiradar <rohitbiradar18@gmail.com>
2020-09-22 18:06:05 +0530

    RecordForm with layoutType=full

--- force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
+++ force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
@@ -1,11 +1,7 @@
-import { LightningElement, track } from 'lwc';
-import NAME_FIELD from '@salesforce/schema/Account.Name'
-import PHN_FIELD from '@salesforce/schema/Account.Phone';
-import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
+import { LightningElement, track } from 'lwc';;
 export default class AccountRecordForm extends LightningElement {
     
     @track recordId;
-    fieldArray = [NAME_FIELD, PHN_FIELD, WEBSITE_FIELD];
     handleSuccess(event){
         this.recordId = event.detail.id;
     }
