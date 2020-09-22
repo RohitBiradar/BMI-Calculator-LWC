8303f90d259decc507ddd9513643f11e77ac4734
RohitBiradar <rohitbiradar18@gmail.com>
2020-09-22 18:02:03 +0530

    RecordForm with fieldArray

--- force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
+++ force-app/main/default/lwc/accountRecordForm/accountRecordForm.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
-
-export default class AccountRecordForm extends LightningElement {}
+import { LightningElement, track } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Account.Name'
+import PHN_FIELD from '@salesforce/schema/Account.Phone';
+import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
+export default class AccountRecordForm extends LightningElement {
+    
+    @track recordId;
+    fieldArray = [NAME_FIELD, PHN_FIELD, WEBSITE_FIELD];
+    handleSuccess(event){
+        this.recordId = event.detail.id;
+    }
+}
